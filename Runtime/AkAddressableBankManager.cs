447a002c989b86b91a8b1b42f1473e939ac5787a
Murtaza <murtaza@trail.gg>
2023-08-07 11:34:03 +0200

    wait

--- Runtime/AkAddressableBankManager.cs
+++ Runtime/AkAddressableBankManager.cs
@@ -256,7 +256,7 @@ namespace AK.Wwise.Unity.WwiseAddressables
 				}
 			}
 
-			LoadBankAsync(bank, bankData, onBankLoadedCallback, onErrorCallback);
+			LoadBankAsync(bank, bankData, onBankLoadedCallback, onErrorCallback).Wait();
 		}
 
 		public async Task LoadBankAsync(WwiseAddressableSoundBank bank, AssetReferenceWwiseBankData bankData, OnBankLoadedCallback onBankLoadedCallback = null, OnErrorCallback onErrorCallback = null)
